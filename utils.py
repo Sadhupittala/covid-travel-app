66f31f1fb1d5f7e1c6eeccf69812cf7ef255b80c
Sadhupittala <92452452+Sadhupittala@users.noreply.github.com>
2021-12-12 20:00:48 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,9 @@
 import json
 
 def get_country_list():
-    with open("country-codes.json", "r") as f:
-        country_list = json.load(f)
     
-    return country_list
-
 """
-get_country_list: this funcation reads the URL and maps the country to its own code from the site
+get_country_list: this function reads the URL and maps the country to its own code from the site
 Gets the list of country with the codes since each country has a two letter code. this is the code that the API uses
 
 Returns
@@ -15,14 +11,14 @@ Returns
 country_list: dictionary 
    a dictionary where the key is the country name and the value is a code 
 """
+    with open("country-codes.json", "r") as f:
+        country_list = json.load(f)
+    
+    return country_list
+
 
 def get_country(code):
-    country_list = get_country_list()
-    for k,v in country_list.items():
-        if code == v:
-            return k
-    return code
-
+ 
 """get_country: this function returns the country that matches to the given code, if there is no such country it returns the code 
 
     Parameters
@@ -35,13 +31,16 @@ def get_country(code):
     code: str
         returns country name if exsist else return the code 
     """
-    
-def get_code(name):
-    ## gets the two char code
+
     country_list = get_country_list()
-    code = country_list.get(name.lower())
+    for k,v in country_list.items():
+        if code == v:
+            return k
     return code
 
+    
+def get_code(name):
+ 
 """get_code: this function returns the country code that matches  the given country, if there is no such code it returns null 
 
     Parameters
@@ -55,12 +54,13 @@ def get_code(name):
         returns country code if exsist else return null 
     """
 
+    ## gets the two char code
+    country_list = get_country_list()
+    code = country_list.get(name.lower())
+    return code
+
+
 def print_indent(string, bullet=True):
-    indent = "   "
-    if bullet:
-        indent = "   -"
-    print(indent + string)
-    
     """print_indent: this function prints indents with bullets or without according the flag bullet
  
     Parameters
@@ -70,3 +70,9 @@ def print_indent(string, bullet=True):
     bullet: boolean
       a flag represent if we want bullet, the default value is true    
     """
+    indent = "   "
+    if bullet:
+        indent = "   -"
+    print(indent + string)
+    
+ 
